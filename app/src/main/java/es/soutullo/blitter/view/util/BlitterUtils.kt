99f6e1da5559881d96dc3ae5ba941210672f5744
Unknown <samuelsoutullosobral@gmail.com>
2017-08-22 18:09:09 +0200

    Added BillSummaryActivity

--- app/src/main/java/es/soutullo/blitter/view/util/BlitterUtils.kt
+++ app/src/main/java/es/soutullo/blitter/view/util/BlitterUtils.kt
@@ -1,6 +1,7 @@
 package es.soutullo.blitter.view.util
 
 import android.graphics.drawable.Drawable
+import java.util.*
 
 /**
  *
@@ -14,4 +15,6 @@ object BlitterUtils {
     fun generateUserProfilePic(name: String): Drawable {
         TODO("not implemented")
     }
+
+    fun getCurrencySymbol(): String = Currency.getInstance(Locale.getDefault()).symbol
 }
