0125bddb107c30db2a67eb79b900e12a61742eee
Unknown <samuelsoutullosobral@gmail.com>
2017-08-24 17:57:34 +0200

    Implemented database contract and creation

--- app/src/main/java/es/soutullo/blitter/model/BlitterSqlDbContract.kt
+++ app/src/main/java/es/soutullo/blitter/model/BlitterSqlDbContract.kt
@@ -1,4 +1,51 @@
-package DAL
+package es.soutullo.blitter.model
 
+object BlitterSqlDbContract {
+    data class ForeignKey(val currentColumn: TableEntry, val refereeTable: Table, val refereeColumn: TableEntry)
 
-object BlitterSqlDbContract// TODO implement here
+    interface TableEntry {
+        val colName: String
+        val type: String
+        val extraAttributes: String?
+    }
+
+    enum class Table(val tableName: String, val columns: Array<out TableEntry>, vararg val foreignKeys: ForeignKey)  {
+        BILL("bill", BillEntry.values()),
+        BILL_LINE("bill_line", BillLineEntry.values(), ForeignKey(BillLineEntry.BILL_ID, BILL , BillEntry._ID)),
+        PERSON("person", PersonEntry.values()),
+        BILL_PERSON("bill_person", BillPersonEntry.values(), ForeignKey(BillPersonEntry._ID, PERSON, PersonEntry._ID)),
+        RECENT_PERSON("recent_person", RecentPersonEntry.values(), ForeignKey(RecentPersonEntry._ID, PERSON, PersonEntry._ID))
+    }
+
+    enum class BillEntry(override val colName: String, override val type: String, override val extraAttributes: String? = null) : TableEntry {
+        _ID("_id", "INTEGER", "PRIMARY KEY"),
+        NAME("name", "TEXT"),
+        PRICE_WITHOUT_TIP("price_without_tip", "REAL"),
+        TIP_PERCENT("tip_percent", "REAL"),
+        DATE("date", "INTEGER"),
+        SOURCE("source", "INTEGER"),
+        STATUS("status", "INTEGER")
+    }
+
+    enum class BillLineEntry(override val colName: String, override val type: String, override val extraAttributes: String? = null) : TableEntry {
+        _ID("_id", "INTEGER", "PRIMARY KEY"),
+        BILL_ID("bill_id", "INTEGER"),
+        LINE_NUMBER("line_number", "INTEGER"),
+        NAME("name", "TEXT"),
+        PRICE("price", "REAL")
+    }
+
+    enum class PersonEntry(override val colName: String, override val type: String, override val extraAttributes: String? = null) : TableEntry {
+        _ID("_id", "INTEGER", "PRIMARY KEY"),
+        NAME("name", "TEXT", "NOT NULL UNIQUE"),
+    }
+
+    enum class BillPersonEntry(override val colName: String, override val type: String, override val extraAttributes: String? = null) : TableEntry {
+        _ID("_id", "INTEGER", "PRIMARY KEY")
+    }
+
+    enum class RecentPersonEntry(override val colName: String, override val type: String, override val extraAttributes: String? = null) : TableEntry {
+        _ID("_id", "INTEGER", "PRIMARY KEY"),
+        LAST_DATE("last_date", "INTEGER")
+    }
+}
--- app/src/main/java/es/soutullo/blitter/model/dao/sql/helper/BlitterSqlDbHelper.kt
+++ app/src/main/java/es/soutullo/blitter/model/dao/sql/helper/BlitterSqlDbHelper.kt
@@ -3,17 +3,41 @@ package es.soutullo.blitter.model.dao.sql.helper
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import es.soutullo.blitter.model.BlitterSqlDbContract
 
-/**
- *
- */
-class BlitterSqlDbHelper(context: Context) : SQLiteOpenHelper(context, "Bills.db", null, 1) {
+class BlitterSqlDbHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+    companion object {
+        val DB_VERSION = 1
+        val DB_NAME = "Bills.db"
+    }
 
-    override fun onCreate(sqLiteDatabase: SQLiteDatabase) {
-
+    override fun onCreate(db: SQLiteDatabase) {
+        BlitterSqlDbContract.Table.values().forEach({table ->  db.execSQL(this.createTableQuery(table))})
     }
 
     override fun onUpgrade(sqLiteDatabase: SQLiteDatabase, i: Int, i1: Int) {
 
+    }
+
+    /**
+     * Generates the creation sql query for a specific table
+     * @param table The table to create the query for
+     * @return The query as String
+     */
+    private fun createTableQuery(table: BlitterSqlDbContract.Table): String {
+        val queryBuilder = StringBuilder()
+
+        queryBuilder.append(String.format("CREATE TABLE %s ( ", table.tableName))
+
+        for (column in table.columns) {
+            val extraAttributes = column.extraAttributes ?: "NOT NULL"
+            queryBuilder.append(String.format("%s %s %s,", column.colName, column.type, extraAttributes))
+        }
+
+        for ((currentCol, refTable, refCol) in table.foreignKeys) {
+            queryBuilder.append(String.format("FOREIGN KEY (%s) REFERENCES %s(%s),", currentCol, refTable, refCol))
+        }
+
+        return queryBuilder.replace(queryBuilder.lastIndexOf(","), queryBuilder.lastIndexOf(",") + 1, ");").toString()
     }
 }
