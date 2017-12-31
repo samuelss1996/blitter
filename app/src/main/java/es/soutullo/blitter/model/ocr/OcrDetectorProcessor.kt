847c829a864fee164b40a6bb7dccfccda1b07c49
Unknown <samuelsoutullosobral@gmail.com>
2017-12-31 17:43:45 +0100

    Some improvements to the recognition algorithm

--- app/src/main/java/es/soutullo/blitter/model/ocr/OcrDetectorProcessor.kt
+++ app/src/main/java/es/soutullo/blitter/model/ocr/OcrDetectorProcessor.kt
@@ -3,6 +3,7 @@ package es.soutullo.blitter.model.ocr
 import android.util.Log
 import android.util.SparseArray
 import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.Text
 import com.google.android.gms.vision.text.TextBlock
 import es.soutullo.blitter.R
 import es.soutullo.blitter.model.vo.bill.Bill
@@ -19,6 +20,7 @@ import kotlin.math.abs
 // TODO http://i.imgur.com/gyGY1TP.jpg - zero price and one product not on the very left
 class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val overlay: GraphicOverlay<OcrGraphic>) : Detector.Processor<TextBlock> {
     private val countedSamples = mutableMapOf<RecognizedData, Int>()
+    private var successfulScans = 0
 
     override fun receiveDetections(detections: Detector.Detections<TextBlock>?) {
         val items = detections?.detectedItems
@@ -35,8 +37,9 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
             if(pricesBlock != null) {
                 this.findProducts(items, pricesBlock)?.let { recognizedData ->
                     val sampleCount = this.countedSamples.getOrDefault(recognizedData, 0)
+                    this.successfulScans++
 
-                    if(sampleCount >= 1) {
+                    if(sampleCount >= 1 && this.successfulScans > 3) {
                         val bill = this.createBill(recognizedData)
                         this.activity.billRecognized(bill)
                     } else {
@@ -61,14 +64,11 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
 
         var currentBlock = mutableListOf<Product>()
         rightColumnComponents.forEach {
-            val value = it.value.trimDecimalSeparator().trim().split(Regex(" +")).last()
+            val value = it.value.findPriceOrNull()
 
-            if(value.removeNumeric().length <= 3 && value.toPriceOrNull() != null && !it.value.contains(Regex("[#%&/():]"))
-                    && (value.contains(Regex("[.,]")) || value.preserveNumeric().length < 4)) {
-                val name = leftColumnComponents.minBy { nameBlock -> abs(it.boundingBox.top - nameBlock.boundingBox.top) }
-                        ?.value?.removeNumeric() ?: ""
-
-                currentBlock.add(Product(name, value.toPrice()))
+            if(value != null) {
+                val name = this.findProductName(leftColumnComponents, it)
+                currentBlock.add(Product(name, value))
             } else {
                 if(currentBlock.isNotEmpty()) {
                     numericComponentBlocks.add(currentBlock)
@@ -84,19 +84,37 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
         return numericComponentBlocks.maxBy { it.size }
     }
 
+    private fun findProductName(leftColumnComponents: List<Text>, priceBlock: Text): String {
+        val firstCandidate = leftColumnComponents.minBy { nameBlock -> abs(priceBlock.boundingBox.top - nameBlock.boundingBox.top) }?.value
+                //?.value?.removeNumeric() ?: ""
+        val secondCandidate = leftColumnComponents.minBy { nameBlock -> abs(priceBlock.boundingBox.top - nameBlock.boundingBox.bottom) }?.value
+
+        if((firstCandidate?.removeNumeric()?.trim() ?: "") == "") {
+            if((secondCandidate?.removeNumeric()?.trim() ?: "") == "") {
+                return firstCandidate?.trim() ?: ""
+            } else {
+                return secondCandidate?.removeNumeric()?.trim() ?: ""
+            }
+        }
+
+        return firstCandidate?.removeNumeric()?.trim() ?: ""
+    }
+
     private fun findProducts(items: SparseArray<TextBlock>, pricesBlock: List<Product>): RecognizedData? {
         val totalExpectedPrice = pricesBlock.sumByDouble { it.price }
-        val totalReadPrice = this.findTotalPrice(items, totalExpectedPrice)
+        val totalPriceCandidates = this.findTotalPriceCandidates(items, totalExpectedPrice)
 
-        for(topIndex in pricesBlock.size downTo 1) {
-            val validProducts = pricesBlock.subList(0, topIndex)
-            val validProductsSum = validProducts.sumByDouble { it.price }
+        for(totalReadPrice in totalPriceCandidates) {
+            for(topIndex in pricesBlock.size downTo 1) {
+                val validProducts = pricesBlock.subList(0, topIndex)
+                val validProductsSum = validProducts.sumByDouble { it.price }
 
-            for(taxesIndex in 0..5) {
-                val possibleTaxes = pricesBlock.map { it.price }.getOrNull(pricesBlock.size - taxesIndex) ?: 0.0
+                for(taxesIndex in 0..5) {
+                    val possibleTaxes = pricesBlock.map { it.price }.getOrNull(pricesBlock.size - taxesIndex) ?: 0.0
 
-                if(totalReadPrice!= null && abs(validProductsSum + possibleTaxes - totalReadPrice) <= 1e-6) {
-                    return RecognizedData(validProducts, possibleTaxes)
+                    if(abs(validProductsSum + possibleTaxes - totalReadPrice) <= 1e-6) {
+                        return RecognizedData(validProducts, possibleTaxes)
+                    }
                 }
             }
         }
@@ -108,7 +126,7 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
         val bill = Bill(null, this.activity.getString(R.string.bill_uncompleted_default_name), Date(), EBillSource.CAMERA, EBillStatus.UNCONFIRMED)
 
         recognizedData.products.forEachIndexed { index, product ->
-            bill.addLine(BillLine(null, bill, index, product.name, product.price.toFloat()))
+            bill.addLine(BillLine(null, bill, index, product.name.take(21), product.price.toFloat()))
         }
 
         if(recognizedData.taxes > 0) {
@@ -118,10 +136,9 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
         return bill
     }
 
-    private fun findTotalPrice(items: SparseArray<TextBlock>, totalExpectedPrice: Double): Double? {
-        return (0 until items.size()).map { items.valueAt(it) }.flatMap { it.components }.map { it.value.trimDecimalSeparator() }
-                .filter { it.removeNumeric().length <= 3 }.map { it.toPriceOrNull() ?: -1.0 }
-                .filter { it < 4 * totalExpectedPrice }.max()
+    private fun findTotalPriceCandidates(items: SparseArray<TextBlock>, totalExpectedPrice: Double): List<Double> {
+        return (0 until items.size()).map { items.valueAt(it) }.flatMap { it.components }
+                .mapNotNull { it.value.findPriceOrNull() }.filter { it < 4 * totalExpectedPrice }.sortedDescending().take(2)
     }
 
     private fun drawOverlay(items: SparseArray<TextBlock>?) {
@@ -138,12 +155,16 @@ class OcrDetectorProcessor(private val activity: OcrCaptureActivity, private val
         this.overlay.clear()
     }
 
-    private fun String.removeNumeric() = this.replace(Regex("[0-9,.]"), "")
-    private fun String.preserveNumeric() = this.replace(Regex("[^0-9,.]"), "")
+    private fun String.removeNumeric() = this.replace(Regex("[0-9,.]"), "").trim()
+    private fun String.preserveNumeric() = this.replace(Regex("[^0-9,.]"), "").trim()
     private fun String.trimDecimalSeparator() = this.replace(Regex(" *([,.]) *"), {result ->  result.groupValues[1] })
     private fun String.toPrice() = this.toPriceOrNull()!!
     private fun String.toPriceOrNull() = this.preserveNumeric().toDoubleOrNull() ?: this.replace(",", "#")
             .replace(".", ",").replace("#", ".").preserveNumeric().toDoubleOrNull()
+
+    private fun String.findPriceOrNull() = this.trimDecimalSeparator().trim().split(Regex(" +"))
+            .lastOrNull { it.toPriceOrNull() != null && it.removeNumeric().length < 3 && !it.contains(Regex("[#%&/():]"))
+                && (it.contains(Regex("[.,]")) || it.preserveNumeric().length < 4)}?.toPrice()
 
     private data class RecognizedData(val products: List<Product>, val taxes: Double)
 
