a82be6233614c68c33ba8305ce1db4a62b3bcad5
Unknown <samuelsoutullosobral@gmail.com>
2018-03-05 17:23:18 +0100

    Added JavaDoc

--- app/src/main/java/es/soutullo/blitter/view/util/BillBitmapGenerator.kt
+++ app/src/main/java/es/soutullo/blitter/view/util/BillBitmapGenerator.kt
@@ -10,6 +10,12 @@ import es.soutullo.blitter.model.vo.bill.Bill
 import java.io.File
 import java.io.FileOutputStream
 
+/**
+ * Generates the shareable receipt image
+ * @param context The Android context
+ * @param bill The bill object to be converted to bitmap
+ * @param includeDetails Indicates whether or not the detailed breakdown for each person should be included
+ */
 class BillBitmapGenerator(private val context: Context, private val bill: Bill, private val includeDetails: Boolean) {
     companion object{
         const val RECEIPT_WIDTH = 428
@@ -28,6 +34,10 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
     private lateinit var canvas: Canvas
     private lateinit var typeface: Typeface
 
+    /**
+     * Generates the receipt image and returns an URI, so it can be shared
+     * @return The URI
+     */
     fun generateBillBitmap(): Uri {
         val cachePath = File(this.context.cacheDir, "images")
         val bitmap = Bitmap.createBitmap(RECEIPT_WIDTH, this.receiptHeight(), Bitmap.Config.ARGB_8888)
@@ -46,11 +56,16 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         return FileProvider.getUriForFile(this.context, "es.soutullo.blitter.fileprovider", newFile)
     }
 
+    /**
+     * Prepares de file system to write the temporary image
+     * @param cachePath The path of the folder where te temporary image should be stored
+     */
     private fun prepareOutputStream(cachePath: File): FileOutputStream {
         cachePath.mkdirs()
         return FileOutputStream("$cachePath/image.png")
     }
 
+    /** Draws the receipt over the bitmap */
     private fun drawReceipt() {
         this.drawBackground()
         this.drawBlitterTitle()
@@ -70,6 +85,7 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         this.drawBreakdown()
     }
 
+    /** Draws the white background of the receipt */
     private fun drawBackground() {
         val backgroundPaint = Paint()
 
@@ -77,20 +93,28 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         this.canvas.drawRect(0f, 0f, RECEIPT_WIDTH.toFloat(), this.receiptHeight().toFloat(), backgroundPaint)
     }
 
+    /** Draws the receipt title */
     private fun drawBlitterTitle() {
         this.drawText(this.context.getString(R.string.bill_summary_header), true, android.graphics.Paint.Align.CENTER, 24f, 8f)
     }
 
+    /** Draws a thin ASCII char separator over the receipt */
     private fun drawThinSeparator(marginTop: Float) {
         val separator = this.context.getString(R.string.bill_summary_separator_product_count).repeat(SEPARATORS_WIDTH)
         this.drawText(separator, true, Paint.Align.CENTER, marginTop = marginTop)
     }
 
+    /** Draws the product count line */
     private fun drawProductsCount() {
         this.drawText(this.context.getString(R.string.bill_summary_total_products_text), true, Paint.Align.LEFT)
         this.drawText(this.bill.lines.size.toString(), false, Paint.Align.RIGHT)
     }
 
+    /**
+     * Draws a header over the receipt
+     * @param headerText The text to be included on the header
+     * @param marginTop The top margin of this header
+     */
     private fun drawHeader(headerText: String, marginTop: Float) {
         val separator = this.context.getString(R.string.bill_summary_separator_products_title).repeat(SEPARATORS_WIDTH)
 
@@ -98,6 +122,7 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         this.drawText(separator, true, Paint.Align.CENTER)
     }
 
+    /** Draws the products section (products list) */
     private fun drawProducts() {
         this.bill.lines.forEach {
             this.drawText(it.name.toUpperCase(), true, Paint.Align.LEFT)
@@ -105,11 +130,13 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         }
     }
 
+    /** Draws a thick ASCII char separator over the receipt */
     private fun drawThickSeparator(marginTop: Float) {
         val separator = this.context.getString(R.string.bill_summary_separator_total_price).repeat(SEPARATORS_WIDTH)
         this.drawText(separator, true, Paint.Align.CENTER, marginTop = marginTop)
     }
 
+    /** Draws the total price block over the receipt */
     private fun drawTotal() {
         if(this.bill.tax > 0) {
             this.drawText(this.context.getString(R.string.bill_summary_subtotal_text), true, Paint.Align.LEFT)
@@ -128,6 +155,7 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         }
     }
 
+    /** Draws the payment breakdown (how much should each person pay) over the receipt */
     private fun drawBreakdown() {
         this.persons().forEachIndexed { index, it ->
             this.drawText(it.name.toUpperCase(), true, Paint.Align.LEFT, marginTop = if(this.includeDetails && index > 0) 14f else 0f)
@@ -152,6 +180,14 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         }
     }
 
+    /**
+     * Draws generic text over the receipt
+     * @param text The text to be drawn
+     * @param drawOnNewLine Starts to draw on a new line, considering the last drawn text
+     * @param align The alignment of the text to be drawn
+     * @param size The size of the text to be drawn
+     * @param marginTop The margin top of the text block to be drawn
+     */
     private fun drawText(text: String, drawOnNewLine: Boolean, align: Paint.Align, size: Float = 16f, marginTop: Float = 0f) {
         this.lastTextLinePosition += if(drawOnNewLine) size + marginTop else marginTop
         val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -170,6 +206,7 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         this.canvas.drawText(text, horizontalPosition, this.lastTextLinePosition, textPaint)
     }
 
+    /** Calculates the receipt image height for the current bill */
     private fun receiptHeight(): Int {
         var totalPriceHeight = if(this.bill.tax > 0) TAXED_TOTAL_PRICE_HEIGHT else STANDARD_TOTAL_PRICE_HEIGHT
         val productsHeight = this.bill.lines.size * LINE_HEIGHT
@@ -184,5 +221,6 @@ class BillBitmapGenerator(private val context: Context, private val bill: Bill, 
         return HEADER_HEIGHT + productsHeight + totalPriceHeight + BREAKDOWN_HEADER_HEIGHT + breakdownHeight + detailedBreakdownHeight
     }
 
+    /** @return The persons included in this bill's breakdown */
     private fun persons() = this.bill.lines.map { it.persons }.flatten().distinct()
 }
