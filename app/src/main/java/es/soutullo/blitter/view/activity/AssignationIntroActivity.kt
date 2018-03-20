39b0d933441917082c91cec305dc4007d8c7a24c
Unknown <samuelsoutullosobral@gmail.com>
2018-03-20 02:35:43 +0100

    Added strings for Portuguese

--- app/src/main/java/es/soutullo/blitter/view/activity/AssignationIntroActivity.kt
+++ app/src/main/java/es/soutullo/blitter/view/activity/AssignationIntroActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import es.soutullo.blitter.R
 import es.soutullo.blitter.model.vo.bill.Bill
 
